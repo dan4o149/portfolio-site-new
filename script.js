312c2e449d391fee4d6e062046f06cf5b255b6de
dan4o149 <230107173@sdu.edu.kz>
2025-07-11 17:44:13 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-document.querySelector('form').addEventListener('submit', function(e) {
-  e.preventDefault();
-  alert('Спасибо! Сообщение отправлено.');
+document.addEventListener("DOMContentLoaded", function () {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector("nav");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("active");
+  });
 });
